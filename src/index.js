dfb56238814b64ed93bdd38d2589541b11adc066
lle <281428701@qq.com>
2019-10-26 10:50:51 +0800

    add demo and other little change

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
-import ChartBar from "./ChartBar";
-import "./style/global.scss";
-
-class Index extends Component {
-    render() {
-        return (
-            <ChartBar scaleDirectionY={"left"} xAxisScaleAlign={"side"} xAxisTxtAlign={"center"} firstScaleY={false} firstScaleX={true}
-                      yAxisTextAlign={"right"} gridDashX={[5, 5, 5]} markLineDash={[2, 4, 2, 4]} barPaddinng={10} markIndex={5}/>
-        );
-    }
-}
-
-ReactDOM.render(<Index/>, document.getElementById("root"));
+import BarCharts from "./components/BarCharts";
+export default BarCharts;
